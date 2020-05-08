425b90d6a49c3e2c5df5c31a8ab095d5dcdf8b70
Arkadiusz-Dudziak <45097934+KrolSkorpi0N@users.noreply.github.com>
2020-05-08 22:11:36 +0200

    commit#7 zasady-gry#1
    [x] dodano zasady przemieszczania się po planszy dla gracza

--- plansza1/plansza1/Form1.cs
+++ plansza1/plansza1/Form1.cs
@@ -10,12 +10,15 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System;
 using System.Threading;
+using System.Text.RegularExpressions;
+
 namespace plansza1
 {
 
     public partial class Form1 : Form
     {
         List<List<int>> listArrays = new List<List<int>>();
+        List<Tuple<int, int>> listClick = new List<Tuple<int, int>>(); // Przechowuje klikniete przez gracza pola w sesji [i,j]
         int nRows = 0;
         int nColumns = 0;
         bool Funkcja()
@@ -46,7 +49,7 @@ namespace plansza1
                 MessageBox.Show("Bląd wczytywania pliku");
                 return false;
             }
-                
+
 
             for (int i = 0; i < listLines.Length; i++)
             {
@@ -78,7 +81,7 @@ namespace plansza1
             {
                 //Application.Exit();
                 return;
-            } 
+            }
             InitializeComponent(nRows, nColumns);
             //Thread.Sleep(2000);
 
@@ -88,7 +91,7 @@ namespace plansza1
         {
             int startposition = 100;
             int endposition = 10;
-            for(int i=0; i< listArrays.Count; i++)
+            for (int i = 0; i < listArrays.Count; i++)
             {
                 for (int j = 0; j < listArrays[0].Count; j++)
                 {
@@ -97,34 +100,90 @@ namespace plansza1
                     endposition += 100;
                     l.Click += new System.EventHandler(this.labelClick);
                 }
-                
+
             }
         }
 
         void labelClick(object sender, EventArgs e)
         {
             Label currentlable = (Label)sender;
-            MessageBox.Show(currentlable.Name);
+            Regex regex = new Regex(@"\d+");
+            MatchCollection matches = regex.Matches(currentlable.Name);
+            int j = int.Parse(matches[0].Value);
+            int i = int.Parse(matches[1].Value);
+
+            if (listClick == null)
+                listClick.Add(Tuple.Create(j, i));
+
+            int counter = 0, size = 0;
+            var t = Tuple.Create(j, i);
+            foreach (var item in listClick)
+            {
+                if (item.Item1.ToString() == t.Item1.ToString() && item.Item2.ToString() == t.Item2.ToString())
+                    counter++;
+                size++;
+            }
+
+
+            if (listArrays[i][j] == -1)
+            {
+                MessageBox.Show("Ruch niedozwolony!");
+            }
+            else
+            {
+                if (counter == 0)
+                {
+                    if (size > 0)
+                    {
+                        if (listClick.ElementAt(size - 1).Item1 != t.Item1 && listClick.ElementAt(size - 1).Item2 != t.Item2) //ruch na skos
+                        {
+                            MessageBox.Show("Ruch niedozwolony!");
+                        }
+                        else
+                        {
+                            if ((listClick.ElementAt(size - 1).Item1 - t.Item1 > 1 || listClick.ElementAt(size - 1).Item1 - t.Item1 < -1) || (listClick.ElementAt(size - 1).Item2 - t.Item2 > 1 || listClick.ElementAt(size - 1).Item2 - t.Item2 < -1))// mozna sprobowac skrocic pozniej
+                            {// warunek na nie przeskakiwanie o wiecej niz 1 pole w wysokosci lub w bok
+                                MessageBox.Show("Ruch niedozwolony!");
+                                //Console.WriteLine("listClick: " + listClick.ElementAt(size - 1).Item1 + " " + listClick.ElementAt(size - 1).Item2);
+                                //Console.WriteLine("t: " + t.Item1 + " " + t.Item2);
+                            }
+                            else
+                            {
+                                listClick.Add(Tuple.Create(j, i));
+                                currentlable.BackColor = System.Drawing.Color.Green;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        listClick.Add(Tuple.Create(j, i));
+                        currentlable.BackColor = System.Drawing.Color.Green;
+                    }
+                }
+                else
+                    MessageBox.Show("Pole zostalo odwiedzone!");
+            }
+
         }
 
         Label addlabel(int i, int j, int start, int end)
         {
             Label l = new Label();
-            l.Name = "label" + i.ToString();
-            if (listArrays[i][j]==-1)
+            l.Name = "j:" + j.ToString() + "i:" + i.ToString();
+            if (listArrays[i][j] == -1)
             {//czarny
                 l.BackColor = Color.Black;
             }
             else
             {
                 l.BackColor = Color.White;
-                if (listArrays[i][j] !=0)
+                if (listArrays[i][j] != 0)
                 {
                     l.Text = listArrays[i][j].ToString();
                 }
             }
             l.ForeColor = Color.Black;
-            
+
             l.Font = new Font("Serif", 24, FontStyle.Bold);
             l.Width = 80;
             l.Height = 80;
